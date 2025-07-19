d982c6601467511a7959af59ba4e55f2460e9e33
tobias <mail@tobiasliese.me>
2025-07-19 22:54:49 +0200

    added css dep

--- grammar.js
+++ grammar.js
@@ -12,7 +12,7 @@
 const DIGITS = token(choice('0', seq(/[1-9]/, optional(seq(optional('_'), sep1(/[0-9]+/, /_+/))))));
 const DECIMAL_DIGITS = token(sep1(/[0-9]+/, '_'));
 const HEX_DIGITS = token(sep1(/[A-Fa-f0-9]+/, '_'));
-
+cosnt css = require("../tree-sitter-css/grammar_prefixed")
 /* eslint-disable no-multi-spaces */
 
 const PREC = {
@@ -44,7 +44,7 @@ const PREC = {
 
 /* eslint-enable no-multi-spaces */
 
-module.exports = grammar({
+module.exports = grammar(css, {
   name: 'java',
 
   extras: $ => [
